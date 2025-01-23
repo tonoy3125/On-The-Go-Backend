42412623d64142d57a2e5188431ca5b9b8846eba
tonoy3125 <shaifshajedt@gmail.com>
2025-01-23 18:57:21 +0600

    notFound added

---
+++ src/app/errors/notFound.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response } from "express";
+
+const NotFound = (req: Request, res: Response) => {
+  res.status(404).json({
+    success: true,
+    statusCode: 404,
+    message: "Not Found",
+  });
+};
+
+export default NotFound;

26771e6a59c3e7967af5064e98e1d7b347b30ac1
tonoy3125 <shaifshajedt@gmail.com>
2025-01-23 18:32:59 +0600

    intital project setup

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+
+
+import NotFound from "./app/errors/notFound";
+import { routes } from "./app/routes";
+
+const app: Application = express();
+// const port = 3000
+
+// parser
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// Application Routes
+app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Wellcome to On The Go Travel Story!!!",
+  });
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+app.use(globalErrorHandler);
+
+// Not found
+app.use(NotFound);
+
+export default app;

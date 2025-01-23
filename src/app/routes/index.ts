f4efe5dc491a1b9bc1c962187e4121e013167439
tonoy3125 <shaifshajedt@gmail.com>
2025-01-24 02:26:55 +0600

    user sign up service controller added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import { UserRoutes } from '../modules/user/user.route'
+import { AuthRoutes } from '../modules/auth/auth.route'
 
 const router = express.Router()
 
 const routerModules = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
   {
     path: '/users',
     route: UserRoutes,

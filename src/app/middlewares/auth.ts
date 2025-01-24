66a18cd810b498a2c5dc506ede136e936fecbe1c
tonoy3125 <shaifshajedt@gmail.com>
2025-01-24 16:43:35 +0600

    auth middle ware added

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express'
+
+import httpStatus from 'http-status'
+import { TUserRoles } from '../modules/user/user.interface'
+import config from '../config'
+import { User } from '../modules/user/user.model'
+
+const auth = (...requiredRoles: TUserRoles[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(' ')[1]
+
+      if (!token) {
+        throw new Error()
+      }
+
+      const decoded = jwt.verify(
+        token as string,
+        config.jwt_access_secret as string,
+      ) as JwtPayload
+      const { email, role } = decoded
+
+      const user = await User.findOne({ email, role })
+
+      if (!user) {
+        throw new Error()
+      }
+      // console.log(user)
+
+      if (!requiredRoles.includes(role)) {
+        throw new Error()
+      }
+      req.user = {
+        _id: user._id,
+        email,
+        role,
+      }
+
+      next()
+    } catch (err) {
+      res.status(404).json({
+        success: false,
+        statusCode: httpStatus.UNAUTHORIZED,
+        message: 'You have no access to this route',
+      })
+    }
+  }
+}
+
+export default auth

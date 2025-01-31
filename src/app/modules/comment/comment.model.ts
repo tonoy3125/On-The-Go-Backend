52f49b3f5739545471fbf68af1848c46ec5895d6
tonoy3125 <shaifshajedt@gmail.com>
2025-01-31 16:44:45 +0600

    comment model added

---
+++ src/app/modules/comment/comment.model.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from 'mongoose'
+import { TComment } from './comment.validation'
+
+const commentSchema = new Schema<TComment>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Post',
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+)
+
+export const Comment = model<TComment>('Comment', commentSchema)
